514c361eed1471877151b1a65ea02e30cc5fe133
khardrix <carmenandryanhuffman@yahoo.com>
2020-06-11 21:27:54 -0400

    added svg logo

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ import About from './pages/About.jsx';     // To access the About Us page
 import Contact from './pages/Contact.jsx'; // To access the Contact Us page
 import Home from './pages/Home.jsx';       // To access the Home page
 import ResponsiveNavigation from './components/ResponsiveNavigation.jsx' // responsive toolbar
-import logo from './logo.svg';             // The React logo
+import pleasantHomesLogo from './pleasantHomesLogo_NO_BACKGROUND.svg'
+// import logo from './logo.svg';             // The React logo
 import './App.css';                        // CSS styling file
 
 
 // This is a FUNCTIONAL COMPONENT
 function App() {
+    // Pleasant Homes logo
+    // const pleasantHomesLogo = 
+
     // Array used with the reach router to display the name of pages, path and icons
     const navLinks = [
         {
             text: 'Home',
             path: '/',
-            icon: 'NO_BACKGROUND_ICON_home.png'
+            icon: 'NO_BACKGROUND_ICON_home'
         },
         {
             text: 'Contact',
             path: '/contact',
-            icon: 'NO_BACKGROUND_ICON_home.png'
+            icon: 'NO_BACKGROUND_ICON_home'
         },
         {
             text: 'About',
             path: '/about',
-            icon: 'NO_BACKGROUND_ICON_home.png'
+            icon: 'NO_BACKGROUND_ICON_home'
         }
     ]
 
@@ -33,7 +37,7 @@ function App() {
         <div className="App">
             <ResponsiveNavigation
                 navLinks={ navLinks }
-                logo={ logo }
+                logo={ pleasantHomesLogo }
                 background="#fff"
                 hoverBackground="#ddd"
                 linkColor="#777"

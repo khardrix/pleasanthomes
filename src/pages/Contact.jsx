980df2d3b587838d8d2297f3529e533f27c6b39a
khardrix <carmenandryanhuffman@yahoo.com>
2020-06-18 22:46:58 -0400

    Adding contact form to Contact - MID DEVELOPMENT

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Form from '../components/form/Form.component';
+import forest from '../components/img/forest-during-dawn.jpg';
+import '../images/Smith_House.png';
+import '../assets/styles/contact.styles.css';
 
 class Contact extends Component {
     render() {
         return(
-            <div>Contact Us</div>
+            <div className="contact-div" style={{ backgroundImage:`url(${forest})` }}>
+                <h1>Contact Us</h1>
+                <p>Phone Number: (812)637-1058</p>
+                <Form />
+            </div>
         )
     }
 }
